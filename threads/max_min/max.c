f2986bbec35e4d1b760edfafc80ca3a78560cf7b
99003511 <78534933+99003511@users.noreply.github.com>
2021-03-04 07:03:19 +0530

    Update max.c

--- threads/max_min/max.c
+++ threads/max_min/max.c
@@ -4,10 +4,11 @@ int Array[1000] = {0}, maxarr[10] = {0};
 
 void *task_body_max(void *pv)
 {
-  int i, thread_num = (__intptr_t)pv, tempmax = 0;  
+  int i, thread_num = (__intptr_t)pv,  tempmax = 0;  
   for(i=0; i<100; ++i)
   {
-    /* Searching maximum in 100 array values*/
+    //Searching maximum in 100 array values
+    
     if(Array[thread_num * 100 + i] > tempmax)
     {
       tempmax = Array[i];
@@ -25,12 +26,13 @@ int findmax()
 
   for(i=0; i<1000; i++)
   {
-    /* Assigning random value to Array */
+    //Assigning random value to the Array 
     Array[i] = random()%100;
   }
   for(i=0; i<n ; ++i)
   {
-    /* Create a thread to search 100 array values */
+    // Create a thread to search 100 array values 
+    
     pthread_create(&ptrarr[i], NULL, task_body_max, (void*)(__intptr_t) i);
   }
   for(i=0; i<n ; ++i)
@@ -39,7 +41,7 @@ int findmax()
   }
   for(i=0; i < n; ++i)
   {
-    /* Find Minimum from thread results*/
+    // Find Minimum from thread results
     if(maxarr[i] > tempmax)
     {
       tempmax = maxarr[i];
