f9ca0d62655c85d2e0225bd296847394e9c795f7
99003511 <78534933+99003511@users.noreply.github.com>
2021-03-04 07:07:13 +0530

    Update min.c

--- threads/max_min/min.c
+++ threads/max_min/min.c
@@ -1,13 +1,14 @@
-#include "MaxMin.h"
+ #include "MaxMin.h"
 
-int MinArray[1000] = {0}, minarr[10] = {0};
+ int MinArray[1000] = {0}, minarr[10] = {0};
 
-void *task_body_min(void *pv)
-{
+ void *task_body_min(void *pv)
+ {
   int i, thread_num = (__intptr_t)pv, tempmin = 0;  
   for(i=0; i<100; ++i)
   {
-    /* Searching minimum in 100 array values*/
+    // to Search minimum in 100 array values
+    
     if(MinArray[thread_num * 100 + i] < tempmin)
     {
       tempmin = MinArray[i];
@@ -25,12 +26,13 @@ int findmin()
 
   for(i=0; i<1000; i++)
   {
-    /* Assigning random value to Array */
+    //to Assign random value to Array 
+    
     MinArray[i] = random()%100;
   }
   for(i=0; i<n ; ++i)
   {
-    /* Create a thread to search 100 array values */
+    // create a thread to search 100 array values 
     pthread_create(&ptrarr[i], NULL, task_body_min, (void*)(__intptr_t) i);
   }
   for(i=0; i<n ; ++i)
@@ -39,7 +41,7 @@ int findmin()
   }
   for(i=0; i < n; ++i)
   {
-    /* Find Maximum from thread results*/
+    // to Find Maximum from thread results
     if(minarr[i] < tempmin)
     {
       tempmin = minarr[i];
