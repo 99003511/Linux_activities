58e17a8d4e736adf63b8aeba3f7f6d38289cf040
99003511 <78534933+99003511@users.noreply.github.com>
2021-03-04 07:04:37 +0530

    Update maxmin.h

--- threads/max_min/maxmin.h
+++ threads/max_min/maxmin.h
@@ -1,13 +1,15 @@
-#ifndef __MAXMIN_H
-#define __MAXMIN_H
+ #ifndef __MAXMIN_H
+ #define __MAXMIN_H
 
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
+ #include <pthread.h>
+ #include <stdio.h>
+ #include <stdlib.h>
 
-/* Finding maximum number in Array*/
+    // to Find maximum number in Array
 int findmax();
-/* Finding minimum number in Array */
+
+// to Find minimum number in Array 
+
 int findmin();
 
 #endif
