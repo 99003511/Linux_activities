2e2e5f5834755f03efa044ca772a9d24cffb09c9
99003511 <78534933+99003511@users.noreply.github.com>
2021-03-04 07:18:43 +0530

    Update time.c

--- threads/time/time.c
+++ threads/time/time.c
@@ -5,9 +5,9 @@ void *task_body_time(void* pv)
   int i=0;
   time_t t;
   struct tm* current_time;
-  /* Get current time in seconds */
+  //to Get current time in seconds 
   t = time(NULL);
-  /* Change the format to hh : mm : ss*/
+  // to Change the format to hh : mm : ss
   current_time = localtime(&t);
   printf("%02d : %02d : %02d \n",current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
 
@@ -19,9 +19,9 @@ void getTime()
 
   for(i=0; i<n; ++i)
   {
-    /* Create a thread to get current time */
+    // to Create a thread to get current time 
     pthread_create(&ptrarr[i], NULL, task_body_time, (void *)(__intptr_t)i);
-    /* Sleep for 3 seconds */
+    // to Sleep for 3 seconds 
     sleep(3);
   }
   for(i=0; i<n; i++)
