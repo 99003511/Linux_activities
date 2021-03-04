c36ec50a3d4d3ee0ff9b849a401067d905014444
99003511 <78534933+99003511@users.noreply.github.com>
2021-03-04 07:13:20 +0530

    Update sum.c

--- threads/sum/sum.c
+++ threads/sum/sum.c
@@ -1,28 +1,28 @@
 #include "sum.h"
 
-int Array[1000] = {};
-int sum[10] = {};
+int arr[1000] = {};
+int summ[10] = {};
 
 void *task_body(void *pv) {
   int i, ps, temp, thread_num = (__intptr_t)pv;
   for (i = 0; i <= 100; i++)
-    /* Adding 100 array values */
+    // Adding 100 array values 
     sum[i] += Array[ thread_num * 100 + i]; 
 }
 
 int sumof1000() {
 
-  int i, k = 0, n = 10, final_sum = 0;
+  int i, k = 0, n = 10, final_summ = 0;
   pthread_t ptarr[n];
   srand(time(0));
 
   for(i=0; i<1000; i++) {
-    /* Assigning random value to Array */
-    Array[i] = random()%10;
+    //Assigning random value to Array 
+    arr[i] = random()%10;
   }
 
   for (i = 0; i < n; i++) {
-    /* Create a thread to add 100 array values */
+    // Create a thread to add 100 array values 
     pthread_create(&ptarr[i], NULL, task_body, (void *)(__intptr_t)i);
   }
 
@@ -31,8 +31,8 @@ int sumof1000() {
 
   for(i=0; i<100; i++)
   {
-    /* Adding the results from thread to get final value */
-    final_sum += sum[i]; 
+    // Adding the results from thread to get final value 
+    final_summ += summ[i]; 
   }
-  return final_sum; 
+  return final_summ; 
 }
